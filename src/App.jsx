2fb111dbf25a55c37eb433c05a3d2e354e15bfd9
Natalie Rekai <ncrekai@gmail.com>
2026-02-24 14:54:40 -0500

    deploy to gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
-// import Rotate from './tests/Rotate';;
-// import Spring from './tests/Spring';
-// import Drag from './tests/Drag';
-// import MotionPath from './tests/MotionPath';
-// import SimpleScroll from './tests/SimpleScroll';
-// import ScrollPath from './tests/ScrollPath';
 import MapContainer from './MapContainer';
 import Header from './Header';
-import TestContainer from './TestContainer';
 
 const App = () => {
   return (
     <div className='container'>
       <Header />
       <MapContainer />
-      {/* <TestContainer /> */}
     </div>
   )
 }
